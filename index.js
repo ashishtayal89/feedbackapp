e6b350b41c8926350aee7c160f3d1043da97e6a0
ashishtayal89 <ashishtayal14@gmail.com>
2020-04-10 19:10:43 +0530

    Added code for Oauth, logout and mongo DB setup

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./config/keys");
-const app = express();
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => console.log(profile)
-  )
-);
+require("./models/User");
+require("./services/passport");
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+const app = express();
+
+app.use(
+  cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
